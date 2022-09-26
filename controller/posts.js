ff73cb2d8d8b7baac5425a49fa05774dc06dadf0
Akshay0231 <akshaay2301@gmail.com>
2022-09-26 19:34:14 +0530

    getPosts logic implemented

--- controller/posts.js
+++ controller/posts.js
@@ -1,3 +1,14 @@
-export const getPosts = (req, res) => {
-    res.send("Home Page Testing only")
+import Post from "../models/postMessages"
+
+export const getPosts = async (req, res) => {
+    try {
+        const posts = await Post.find()
+        res.status(200).json(posts)
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
+}
+
+export const createPost = (req, res) => {
+    res.send('Post Creation logic')
 }
